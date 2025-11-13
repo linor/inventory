a26914cac33f8020f4ea6f0b24158217a5b6510a
Krijn Schaap <krijnschaap@gmail.com>
2025-11-13 16:36:26 +0100

    Redirect to /item or /scanner depending on device type.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
+import MobileDetect from 'mobile-detect';
 
-export default function Home() {
-  redirect('/category');
+export default async function Home() {
+    const userAgent = (await headers()).get('user-agent') || '';
+    const mobileDetect = new MobileDetect(userAgent);
+
+    if (mobileDetect.mobile()) {
+        redirect('/scanner');
+    } else {
+        redirect('/item');
+    }
 }
